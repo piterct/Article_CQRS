8070bc29ccc52984510c782c5e67c78fd5199dbf
Michael Peter <michael_piterct@hotmail.com>
2022-03-31 21:09:06 -0300

    ILikeHandler

---
+++ Article/Article.Domain/Handlers/Interfaces/ILikeHandler.cs
@@ -0,0 +1,11 @@
+﻿using Article.Domain.Commands.Like;
+using Article.Shared.Commands.Contracts;
+using System.Threading.Tasks;
+
+namespace Article.Domain.Handlers.Interfaces
+{
+    public interface  ILikeHandler
+    {
+        ValueTask<ICommandResult> Handle(AddLikeArticleCommand command)
+    }
+}
